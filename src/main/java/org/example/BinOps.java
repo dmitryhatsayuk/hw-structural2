b7626ca1b64cfd8d099929a25871134e71fa2083
dmitryhatsayuk <155708514+dmitryhatsayuk@users.noreply.github.com>
2024-07-02 00:13:56 +0700

    Update

--- src/main/java/org/example/BinOps.java
+++ src/main/java/org/example/BinOps.java
@@ -1,2 +1,24 @@
-package org.example;public class BinOps {
+package org.example;
+
+public class BinOps {
+    public String sum(String a, String b) {
+    /*Переведите числа из двоичной записи в
+      строках в int, сложите и переведите
+      обратно в двоичную запись*/
+        int aInt = Integer.parseInt(a, 2);
+        int bInt = Integer.parseInt(b, 2);
+        return Integer.toBinaryString(aInt + bInt);
+    }
+
+    public String mult(String a, String b) {
+    /*Переведите числа из двоичной записи в
+      строках в int, перемножьте и переведите
+      обратно в двоичную запись*/
+        int aInt = Integer.parseInt(a, 2);
+        int bInt = Integer.parseInt(b, 2);
+        return Integer.toBinaryString(aInt * bInt);
+    }
 }
+//Реализуйте оба метода.
+// Для парсинга из двоичной записи используйте Integer.parseInt(binStr, 2),
+// для конвертации обратно - Integer.toBinaryString.
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BinOps bins = new BinOps();
+        System.out.println(bins.sum("1000110101010", "1010010111110"));
+        System.out.println(bins.sum("1", "1"));
+        System.out.println(bins.mult("0", "1"));
+        System.out.println(bins.mult("101101010101010", "10000100010001"));
     }
 }
